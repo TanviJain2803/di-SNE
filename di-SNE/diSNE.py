cf3de06293a0b23a6bee620be53a5d2476cdd6d4
ktychn <146883712+ktychn@users.noreply.github.com>
2024-05-17 11:54:40 -0700

    Update diSNE.py

--- di-SNE/diSNE.py
+++ di-SNE/diSNE.py
@@ -12,10 +12,19 @@ def main():
     # output 
     
     # other options
-#     parser.add_argument()
-    
+    # perplexity
+    parser.add_argument("-p","--perplexity", help="perplexity value used in tSNE, recommended range: _", type=int) #float?
+    # number of iterations
+    parser.add_argument("-n","--num-iterations", help="number of iterations used in tSNE, default: _", type=int)
+    # learning rate
+    parser.add_argument("-r","--learning-rate", help="learning rate used in tSNE, default: _", type=float) #int?
     # parse args
     args = parser.parse_args()
+
+#dataset = args.data
+#perplexity = args.perplexity
+#iterations = args.num_iterations
+#learning_rate = args.learning_rate
     
    if __name__ == "__main__":
        main()
